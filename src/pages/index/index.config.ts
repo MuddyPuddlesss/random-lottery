976e458ed9a7c3553d1b8a139c7692b55ff99f7b
JesseX <shallow.wow@gmail.com>
2022-08-01 21:13:12 +0800

    feat: 字段名脱敏

--- src/pages/index/index.config.ts
+++ src/pages/index/index.config.ts
@@ -1,4 +1,4 @@
 export default definePageConfig({
-  navigationBarTitleText: '看你就要走运',
+  navigationBarTitleText: 'Home',
   "usingComponents": {}
 })
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -4,22 +4,22 @@ import { Stepper } from '@taroify/core'
 import './index.less'
 import homeImg from './../../assets/pointer.jpg'
 
-const redpool = Array.from(new Array(34).keys()).slice(1); // red ball 1 - 33
-const bluepool = Array.from(new Array(17).keys()).slice(1); // red ball 1 - 36
+const redpool = Array.from(new Array(34).keys()).slice(1); // 区间 1 - 33
+const bluepool = Array.from(new Array(17).keys()).slice(1); // 区间 1 - 16
 export default class Index extends Component {
   state = {
     showResult: false,
   }
 
   count: number | string = 1
-  lotteryList: any[] = []
+  randomNumsList: any[] = []
 
-  generateANoteLottery = () => {
+  generateANoteRandomNums = () => {
     const redResult = this.randomSelect(redpool, 6)
     const blueResult = this.randomSelect(bluepool, 1)
     return {
-      redBalls: redResult,
-      blueBalls: blueResult
+      redNums: redResult,
+      blueNums: blueResult
     }
   }
 
@@ -34,21 +34,21 @@ export default class Index extends Component {
     return result.sort((a, b) => a - b);
   }
 
-  getLotterys = () => {
+  getRandomNumss = () => {
     this.setState({
       showResult: false
     })
     const { count } = this
-    const lotteryResult: any[] = []
+    const randomNumsResult: any[] = []
     for (let index = 0; index < count; index++) {
-      lotteryResult.push(this.generateANoteLottery())
+      randomNumsResult.push(this.generateANoteRandomNums())
     }
 
-    this.lotteryList = lotteryResult
+    this.randomNumsList = randomNumsResult
     this.setState({
       showResult: true
     })
-    console.log(this.lotteryList)
+    console.log(this.randomNumsList)
   }
 
   render () {
@@ -60,22 +60,22 @@ export default class Index extends Component {
         </View>
         <View className='content'>
           <View className='content-count'>
-            <Text className='content-count-text'>你想选几注(PS:最多5注)</Text>
+            <Text className='content-count-text'>随机数组数(Max: 5)</Text>
             <Stepper size='26' max={5} onChange={(value) => this.count = value} />
           </View>
-          <View className='content-btn' onClick={() => this.getLotterys()}>生成号码</View>
+          <View className='content-btn' onClick={() => this.getRandomNumss()}>生成随机组合</View>
         </View>
         {
           this.state.showResult && (
             <View className='result'>
               {
-                this.lotteryList.map((item, index) => {
+                this.randomNumsList.map((item, index) => {
                   return (
                     <View key={index} className='result-line'>
-                      {item.redBalls.map(red => {
-                        return (<View key={red} className='redBall'>{red}</View>)
+                      {item.redNums.map(red => {
+                        return (<View key={red} className='redNum'>{red}</View>)
                       })}
-                      <View className='blueBall'>{item.blueBalls[0]}</View>
+                      <View className='blueNum'>{item.blueNums[0]}</View>
                     </View>
                   )
                 })
