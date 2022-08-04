7a3e0b117fbbbc6b7de0d5ea04a97f3a6cdd2b2c
JesseX <shallow.wow@gmail.com>
2022-08-04 16:41:43 +0800

    feat: 添加存档记录

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import { View, Image, Text } from '@tarojs/components'
-import { Stepper } from '@taroify/core'
+import Taro from '@tarojs/taro'
+import { Stepper, Button } from '@taroify/core'
 import './index.less'
 import homeImg from './../../assets/pointer.jpg'
 
@@ -9,6 +10,16 @@ const bluepool = Array.from(new Array(17).keys()).slice(1); // 区间 1 - 16
 export default class Index extends Component {
   state = {
     showResult: false,
+    openid: null,
+    isSaved: false,
+  }
+
+  async componentDidMount() {
+    const { result } = await Taro.cloud.callFunction({ name: 'login' })
+    
+    this.setState({
+      openid: result?.openid,
+    })
   }
 
   count: number | string = 1
@@ -36,7 +47,8 @@ export default class Index extends Component {
 
   getRandomNumss = () => {
     this.setState({
-      showResult: false
+      showResult: false,
+      isSaved: false,
     })
     const { count } = this
     const randomNumsResult: any[] = []
@@ -48,7 +60,56 @@ export default class Index extends Component {
     this.setState({
       showResult: true
     })
-    console.log(this.randomNumsList)
+  }
+
+  formatTime(timestamp) {
+    //日期
+    var DD = String(timestamp.getDate()).padStart(2, '0'); // 获取日
+    var MM = String(timestamp.getMonth() + 1).padStart(2, '0'); //获取月份，1 月为 0
+    var yyyy = timestamp.getFullYear(); // 获取年
+
+    // 时间
+    const hh =  String(timestamp.getHours()).padStart(2, '0');       //获取当前小时数(0-23)
+    const mm = String(timestamp.getMinutes()).padStart(2, '0');     //获取当前分钟数(0-59)
+    const ss = String(timestamp.getSeconds()).padStart(2, '0');     //获取当前秒数(0-59)
+    return yyyy + '-' + MM + '-' + DD + ' ' + hh + ':' + mm + ':' + ss;
+  }
+
+  saveRecords() {
+    if (this.state.isSaved) {
+      Taro.showToast({
+        title: '请勿重复存档',
+        icon: 'error',
+        duration: 1000
+      })
+      return;
+    }
+    const db = Taro.cloud.database()
+    const { openid } = this.state
+    const now = new Date()
+    db.collection('records').add({
+      data: {
+        date: now.getTime(),
+        dateString: this.formatTime(now),
+        records: this.randomNumsList,
+        openid: openid
+      }
+    }).then(() => {
+      this.setState({
+        isSaved: true
+      })
+      Taro.showToast({
+        title: '存档成功',
+        icon: 'success',
+        duration: 1000
+      })
+    })
+  }
+
+  jumpToRecords() {
+    Taro.navigateTo({
+      url: `/pages/records/index?id=${this.state.openid}`
+    })
   }
 
   render () {
@@ -83,6 +144,8 @@ export default class Index extends Component {
             </View>
           )
         }
+        {this.state.showResult && <Button size='large' variant='text' color='info' onClick={() => this.saveRecords()}>号码存档</Button>}
+        <View className='record-ball' onClick={() => this.jumpToRecords()}>存档记录</View>
       </View>
     )
   }
--- src/pages/records/index.tsx
+++ src/pages/records/index.tsx
@@ -1,21 +1,62 @@
-import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
+import { Component, useState } from 'react'
 import Taro from '@tarojs/taro'
-
-import type CustomTabBar from '../../custom-tab-bar'
+import { View } from '@tarojs/components'
+import { Loading, Divider } from "@taroify/core"
+import './index.less'
 
 export default class Index extends Component {
-  pageCtx = Taro.getCurrentInstance().page
 
-  componentDidShow () {
-    const tabbar = Taro.getTabBar<CustomTabBar>(this.pageCtx)
-    tabbar?.setSelected(1)
+  state = {
+    isLoading: true,
+  }
+
+  recordsList: any[] = []
+
+  onLoad (options) {
+    const openid = options.id
+    const db = Taro.cloud.database()
+    db.collection('records').where({ openid }).get().then(res => {
+      this.recordsList = res.data.reverse()
+      this.setState({
+        isLoading: false
+      })
+    })
   }
 
   render () {
     return (
-      <View className='index'>
-        <Text>我是购物车！</Text>
+      <View>
+        {this.state.isLoading}
+        {
+          this.state.isLoading
+            ? <Loading size='24px'>加载中...</Loading>
+            : 
+              (<View className='list'>
+                {
+                  this.recordsList.map((record, index) => {
+                    return (
+                      <View key={index} className='result-item'>
+                        <Divider className='result-date'>
+                        {record.dateString}
+                        </Divider>
+                        {
+                          record.records.map((item, idx) => {
+                            return (
+                              <View key={idx} className='result-line'>
+                                {item.redNums.map(red => {
+                                  return (<View key={red} className='redNum'>{red}</View>)
+                                })}
+                                <View className='blueNum'>{item.blueNums[0]}</View>
+                              </View>
+                            )
+                          })
+                        }
+                      </View>
+                    )
+                  })
+                }
+              </View>)
+        }
       </View>
     )
   }
